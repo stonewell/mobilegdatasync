45283fa75fb55323cee80b4dc49840951bb56624
Jingnan Si (jingnan si <Jingnan Si (jingnan.si@gmail.com)>
2010-11-17 17:23:10 +0800

    add volume and ringtone dialog

--- android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/EditBaseActivity.java
+++ android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/EditBaseActivity.java
@@ -1,34 +1,49 @@
 package com.angelstone.android.profileswitcher.ui;
 
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.angelstone.android.profileswitcher.R;
 
-public class EditBaseActivity extends GenericActivity {
+public abstract class EditBaseActivity extends GenericActivity {
 	private static final int[][] OPTION_MENUS = new int[][] {
 			new int[] { R.string.save, android.R.drawable.ic_menu_save },
 			new int[] { R.string.delete, android.R.drawable.ic_menu_delete }, };
 
-	private int mOptionMenuStartPos = 0;
-	
 	public EditBaseActivity() {
 		super(OPTION_MENUS.length);
 	}
 
 	public EditBaseActivity(int optionMenuStartPos) {
 		super(optionMenuStartPos + OPTION_MENUS.length);
-		
-		mOptionMenuStartPos = optionMenuStartPos;
 	}
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
-		if (mOptionMenuStartPos == 0)
+		if (getOptionMenuStartPos() - OPTION_MENUS.length == 0)
 			menu.clear();
 
-		createMenus(menu, 0, OPTION_MENUS);
+		createMenus(menu, getOptionMenuStartPos() - OPTION_MENUS.length, OPTION_MENUS);
 
 		return super.onPrepareOptionsMenu(menu);
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId() - getOptionMenuStartPos() + OPTION_MENUS.length) {
+		case 0: {
+			saveContent();
+			break;
+		}
+		case 1: {
+			deleteContent();
+			break;
+		}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		return true;
+	}
+	
+	protected abstract void saveContent();
+	protected abstract void deleteContent();
 }
--- android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/ProfileEditActivity.java
+++ android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/ProfileEditActivity.java
@@ -1,7 +1,11 @@
 package com.angelstone.android.profileswitcher.ui;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,8 +19,11 @@ import com.angelstone.android.profileswitcher.store.Profile;
 
 public class ProfileEditActivity extends EditBaseActivity implements
 		OnClickListener {
+	private static final int DIALOG_RING_TONE_VIBRATE_VOLUME = 0;
+	
 	private int mId = -1;
 	private Profile mProfile = null;
+	private int mCurrentEditingId = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +36,9 @@ public class ProfileEditActivity extends EditBaseActivity implements
 
 		mProfile = loadProfile(mId);
 
-		EditText et = (EditText)findViewById(R.id.edit_name);
+		EditText et = (EditText) findViewById(R.id.edit_name);
 		et.setText(mProfile.getName());
-		
+
 		CheckBox cb = (CheckBox) findViewById(R.id.check_gps);
 		cb.setOnClickListener(this);
 		cb.setChecked(mProfile.isGpsConfigured());
@@ -95,6 +102,7 @@ public class ProfileEditActivity extends EditBaseActivity implements
 		if (id == -1)
 			return p;
 
+		// TODO: Load Profile from db
 		return p;
 	}
 
@@ -102,37 +110,65 @@ public class ProfileEditActivity extends EditBaseActivity implements
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.check_gps: {
-			findViewById(R.id.toggle_gps).setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.toggle_gps)
+					.setEnabled(((CheckBox) v).isChecked());
 		}
 			break;
 		case R.id.check_wifi: {
-			findViewById(R.id.toggle_wifi).setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.toggle_wifi).setEnabled(
+					((CheckBox) v).isChecked());
 		}
 			break;
 
 		case R.id.check_bluetooth: {
-			findViewById(R.id.toggle_bluetooth)
-					.setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.toggle_bluetooth).setEnabled(
+					((CheckBox) v).isChecked());
 		}
 			break;
 		case R.id.check_phone: {
-			findViewById(R.id.img_btn_phone)
-					.setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.img_btn_phone).setEnabled(
+					((CheckBox) v).isChecked());
 		}
 			break;
 		case R.id.check_email: {
-			findViewById(R.id.img_btn_email)
-					.setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.img_btn_email).setEnabled(
+					((CheckBox) v).isChecked());
 		}
 			break;
 		case R.id.check_alarm: {
-			findViewById(R.id.img_btn_alarm)
-					.setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.img_btn_alarm).setEnabled(
+					((CheckBox) v).isChecked());
 		}
 			break;
 		case R.id.check_notification: {
-			findViewById(R.id.img_btn_notification)
-					.setEnabled(((CheckBox) v).isChecked());
+			findViewById(R.id.img_btn_notification).setEnabled(
+					((CheckBox) v).isChecked());
+		}
+			break;
+		case R.id.img_btn_notification:
+		case R.id.img_btn_phone:
+		case R.id.img_btn_email:
+		case R.id.img_btn_alarm: {
+			editRingToneAndVolume(v.getId());
+		}
+			break;
+		
+		case R.id.check_volume: {
+			View viewParent = (View)v.getParent();
+			View vv = viewParent.findViewById(R.id.seekbar_volume);
+			vv.setEnabled(((CheckBox) v).isChecked());
+		}
+			break;
+		case R.id.check_vibrate: {
+			View viewParent = (View)v.getParent();
+			View vv = viewParent.findViewById(R.id.toggle_vibrate);
+			vv.setEnabled(((CheckBox) v).isChecked());
+		}
+			break;
+		case R.id.check_ringtone: {
+			View viewParent = (View)v.getParent();
+			View vv = viewParent.findViewById(R.id.spinner_ringtone);
+			vv.setEnabled(((CheckBox) v).isChecked());
 		}
 			break;
 		default:
@@ -149,6 +185,78 @@ public class ProfileEditActivity extends EditBaseActivity implements
 		}
 
 		return ret;
+	}
+
+	@Override
+	protected void saveContent() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void deleteContent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void editRingToneAndVolume(int id) {
+		mCurrentEditingId = id;
+		showDialog(DIALOG_RING_TONE_VIBRATE_VOLUME);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DIALOG_RING_TONE_VIBRATE_VOLUME:
+			// This example shows how to add a custom layout to an AlertDialog
+			LayoutInflater factory = LayoutInflater.from(this);
+			final View ringtoneAndVolumeView = factory.inflate(
+					R.layout.ringtone_and_volume, null);
+
+			//Set onClick properties
+			CheckBox c = (CheckBox)ringtoneAndVolumeView.findViewById(R.id.check_volume);
+			c.setOnClickListener(this);
+			c = (CheckBox)ringtoneAndVolumeView.findViewById(R.id.check_vibrate);
+			c.setOnClickListener(this);
+			c = (CheckBox)ringtoneAndVolumeView.findViewById(R.id.check_ringtone);
+			c.setOnClickListener(this);
+			
+			return new AlertDialog.Builder(this)
+					.setIcon(R.drawable.icon)
+					.setTitle(R.string.ringtone_and_volume)
+					.setView(ringtoneAndVolumeView)
+					.setPositiveButton(android.R.string.ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int whichButton) {
+									updateProfileRingtonVolume(ringtoneAndVolumeView);
+								}
+							})
+					.setNegativeButton(android.R.string.cancel,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int whichButton) {
+								}
+							}).create();
+		}
+		return null;
+	}
+
+	private void updateProfileRingtonVolume(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		super.onPrepareDialog(id, dialog);
+		
+		switch(id) {
+		case DIALOG_RING_TONE_VIBRATE_VOLUME:
+			if (mCurrentEditingId == -1)
+				break;
+			
+			break;
+		}
 	}
 
 }
