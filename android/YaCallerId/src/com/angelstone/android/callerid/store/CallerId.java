56f297277a949b8fcdbea369fc0be3adfb174ef9
Jingnan Si (jingnan si <Jingnan Si (jingnan.si@gmail.com)>
2010-12-22 17:50:30 +0800

    add views

--- android/YaCallerId/src/com/angelstone/android/callerid/store/CallerId.java
+++ android/YaCallerId/src/com/angelstone/android/callerid/store/CallerId.java
@@ -1,25 +1,38 @@
 package com.angelstone.android.callerid.store;
 
+import java.util.Map;
+
 import com.angelstone.android.database.Entity;
 
 public class CallerId extends Entity {
+	public static final String TABLE_NAME = "callers";
+
+	public static final String COL_NUMBER = "number";
+	public static final String COL_DATA = "data";
+	public static final String DEFAULT_SORT_ORDER = COL_DATA + " asc";
 
 	@Override
 	public String getTableName() {
-		// TODO Auto-generated method stub
-		return null;
+		return TABLE_NAME;
 	}
 
 	@Override
 	public String getDefaultSortOrder() {
-		// TODO Auto-generated method stub
-		return null;
+		return DEFAULT_SORT_ORDER;
 	}
 
 	@Override
 	public String getNullableColumnName() {
-		// TODO Auto-generated method stub
-		return null;
+		return COL_DATA;
 	}
 
+	@Override
+	public Map<String, String> getColumnTypeMap() {
+		Map<String, String> mapping = super.getColumnTypeMap();
+
+		mapping.put(COL_NUMBER, "VARCHAR");
+		mapping.put(COL_DATA, "BLOB");
+
+		return mapping;
+	}
 }
