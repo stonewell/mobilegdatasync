db34ec58171ec085b31d4af4197eb700e30f946e
Jingna Si <jingnan.si@gmail.com>
2010-12-26 11:03:16 +0800

    finished caller id edit

--- android/YaCallerId/src/com/angelstone/android/callerid/ui/CallerListViewAdapter.java
+++ android/YaCallerId/src/com/angelstone/android/callerid/ui/CallerListViewAdapter.java
@@ -9,24 +9,29 @@ import android.widget.TextView;
 
 import com.angelstone.android.callerid.R;
 import com.angelstone.android.callerid.store.CallerId;
+import com.angelstone.android.callerid.utils.PhotoLoader;
 
 public class CallerListViewAdapter extends ResourceCursorAdapter {
 	private int mIdxNumber = 0;
-	private int mIdxData = 0;
-	
-	public CallerListViewAdapter(Context context, Cursor c) {
+	private int mIdxId = 0;
+	private PhotoLoader mPhotoLoader = null;
+
+	public CallerListViewAdapter(Context context, Cursor c,
+			PhotoLoader photoLoader) {
 		super(context, R.layout.caller_list_item, c);
-		
+
 		mIdxNumber = c.getColumnIndex(CallerId.COL_NUMBER);
-		mIdxData = c.getColumnIndex(CallerId.COL_DATA);
+		mIdxId = c.getColumnIndex(CallerId.COL_ID);
+		mPhotoLoader = photoLoader;
 	}
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		ImageView iv = (ImageView)view.findViewById(R.id.list_item_img);
-		TextView tv = (TextView)view.findViewById(R.id.list_item_text);
-		
+		ImageView iv = (ImageView) view.findViewById(R.id.list_item_img);
+		TextView tv = (TextView) view.findViewById(R.id.list_item_text);
+
 		tv.setText(cursor.getString(mIdxNumber));
+		mPhotoLoader.loadPhoto(iv, cursor.getLong(mIdxId));
 	}
 
 }
