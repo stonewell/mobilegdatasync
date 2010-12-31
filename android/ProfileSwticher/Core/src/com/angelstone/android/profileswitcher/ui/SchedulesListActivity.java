cb698810a9a2b04f275ba83e381784c9d4abaa04
Jingna Si <jingnan.si@gmail.com>
2010-12-31 09:11:49 +0800

    Change caller id use broadcast PhoneState, instead phone state listener

--- android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/SchedulesListActivity.java
+++ android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/ui/SchedulesListActivity.java
@@ -1,34 +1,112 @@
 package com.angelstone.android.profileswitcher.ui;
 
+import android.app.AlertDialog;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 import com.angelstone.android.profileswitcher.R;
+import com.angelstone.android.profileswitcher.store.Schedule;
 
 public class SchedulesListActivity extends ContentListBaseActivity {
+	private Cursor mCursor;
+
+	private ContentObserver mObserver = new ContentObserver(new Handler()) {
+
+		@Override
+		public void onChange(boolean selfChange) {
+			super.onChange(selfChange);
+			updateView();
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.schedules);
-	}
 
-	@Override
-	protected void clearAllContent() {
-		// TODO Auto-generated method stub
-		
+		ListView v = (ListView) findViewById(R.id.schedule_list);
+
+		mCursor = managedQuery(Schedule.CONTENT_URI, null, null, null, null);
+
+		v.setAdapter(new ScheduleAdapter(this, mCursor));
+
+		registerForContextMenu(v);
+
+		getContentResolver().registerContentObserver(Schedule.CONTENT_URI, true,
+				mObserver);
+
+		updateView();
 	}
 
 	@Override
 	protected void editContent(long id) {
-		// TODO Auto-generated method stub
-		
+		Intent intent = new Intent();
+		intent.setClass(this, ScheduleEditActivity.class);
+		intent.putExtra(ProfileSwitcherConstants.EXTRA_ID, id);
+		startActivity(intent);
+	}
+
+	@Override
+	protected void clearAllContent() {
+		AlertDialog ad = new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setTitle(R.string.clear_all_confirm)
+				.setPositiveButton(android.R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int whichButton) {
+								getContentResolver().delete(Schedule.CONTENT_URI, null, null);
+							}
+						}).setNegativeButton(android.R.string.cancel, null).create();
+		ad.show();
 	}
 
 	@Override
 	protected void deleteContent(long id) {
-		// TODO Auto-generated method stub
-		
+		Uri uri = ContentUris.withAppendedId(Schedule.CONTENT_URI, id);
+
+		getContentResolver().delete(uri, null, null);
 	}
 
+	private void updateView() {
+		mCursor.requery();
+
+		if (mCursor == null || mCursor.getCount() == 0) {
+			findViewById(R.id.empty_message).setVisibility(View.VISIBLE);
+		} else {
+			findViewById(R.id.empty_message).setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	protected void updateOptionMenuItemState(Menu menu) {
+		super.updateOptionMenuItemState(menu);
+
+		int length = menu.size();
+
+		for (int i = 0; i < length; i++) {
+			MenuItem item = menu.getItem(i);
+
+			if (item.getTitle().equals(getString(R.string.clear_all))) {
+				item.setEnabled(mCursor != null && mCursor.getCount() > 0);
+			}
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		getContentResolver().unregisterContentObserver(mObserver);
+	}
 }
