d7f092467ebc93ad45c840cf5076a73e71dc38e5
Jingnan Si (jingnan si <Jingnan Si (jingnan.si@gmail.com)>
2011-01-11 15:58:18 +0800

    check result code in onActivityResult. move business logic code to seperator Handler class for later obsfucation

--- android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/service/ProfileSwitcherService.java
+++ android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/service/ProfileSwitcherService.java
@@ -3,42 +3,23 @@ package com.angelstone.android.profileswitcher.service;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
 
+import com.angelstone.android.profileswitcher.ProfileSwitcherConstants;
 import com.angelstone.android.profileswitcher.R;
+import com.angelstone.android.profileswitcher.store.Schedule;
+import com.angelstone.android.profileswitcher.utils.ProfileSwitcherUtils;
 import com.angelstone.android.utils.HandlerThreadQuiter;
 import com.angelstone.android.utils.PhoneToolsUtil;
 
 public class ProfileSwitcherService extends Service {
-	private class ProfileSwitcherHandler extends Handler {
-
-		public ProfileSwitcherHandler(Looper looper) {
-			super(looper);
-		}
-
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			
-			try{
-				
-			}catch(Throwable t) {
-				
-			}finally {
-				
-			}
-		}
-
-	}
-
 	private final LocationListener mLocationListener = new LocationListener() {
 
 		@Override
@@ -58,16 +39,18 @@ public class ProfileSwitcherService extends Service {
 
 		@Override
 		public void onLocationChanged(Location location) {
-			if (PhoneToolsUtil.isBetterLocation(location, mCurrentLoc)) {
-				//TODO: find if there is a profile to active
-			}
+			Message msg = Message.obtain(mHandler,
+					ProfileSwitcherConstants.MSG_WHAT_LOCATION_CHANGED,
+					location);
+
+			mHandler.sendMessage(msg);
 		}
 	};
 
 	private ProfileSwitcherHandler mHandler;
 	private HandlerThread mHandlerThread;
-	private Location mCurrentLoc;
 	private LocationManager mLocationManager;
+	private ContentObserver mScheduleObserver = null;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -82,16 +65,34 @@ public class ProfileSwitcherService extends Service {
 				android.os.Process.THREAD_PRIORITY_BACKGROUND);
 		mHandlerThread.start();
 
-		mHandler = new ProfileSwitcherHandler(mHandlerThread.getLooper());
+		mHandler = new ProfileSwitcherHandler(this, mHandlerThread.getLooper());
 
 		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-		mCurrentLoc = mLocationManager.getLastKnownLocation(PhoneToolsUtil
-				.getLocProvider(mLocationManager));
-		//TODO: Move time/distance to preference
-		mLocationManager.requestLocationUpdates(
-				PhoneToolsUtil.getLocProvider(mLocationManager), 600 * 1000, 500,
-				mLocationListener); 
+		mLocationManager
+				.requestLocationUpdates(PhoneToolsUtil
+						.getLocProvider(mLocationManager), ProfileSwitcherUtils
+						.getLocationUpdateTimePreference() * 60 * 1000,
+						ProfileSwitcherUtils
+								.getLocationUpdateDistancePreference(),
+						mLocationListener);
+
+		mScheduleObserver = new ContentObserver(mHandler) {
+
+			@Override
+			public void onChange(boolean selfChange) {
+				super.onChange(selfChange);
+
+				requestLoadLocationOnlySchedules();
+			}
+
+		};
+
+		getContentResolver().registerContentObserver(Schedule.CONTENT_URI,
+				true, mScheduleObserver);
+
+		requestLoadLocationOnlySchedules();
+		requestActivateLastKnownProfile();
 	}
 
 	@Override
@@ -112,6 +113,17 @@ public class ProfileSwitcherService extends Service {
 		} catch (VerifyError e) {
 			mHandler.getLooper().quit();
 		}
+
+		mLocationManager.removeUpdates(mLocationListener);
+		getContentResolver().unregisterContentObserver(mScheduleObserver);
+	}
+
+	private void requestLoadLocationOnlySchedules() {
+		mHandler.sendEmptyMessage(ProfileSwitcherConstants.MSG_WHAT_LOAD_LOCATION_ONLY_SCHEDULES);
+	}
+
+	private void requestActivateLastKnownProfile() {
+		mHandler.sendEmptyMessage(ProfileSwitcherConstants.MSG_WHAT_LOAD_LAST_KNOWN_PROFILE);
 	}
 
 }
---
+++ android/ProfileSwticher/Core/src/com/angelstone/android/profileswitcher/utils/ProfileSwitcherUtils.java
@@ -0,0 +1,37 @@
+package com.angelstone.android.profileswitcher.utils;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.angelstone.android.profileswitcher.ProfileSwitcherConstants;
+import com.angelstone.android.profileswitcher.store.Profile;
+
+public class ProfileSwitcherUtils {
+	public static void activateProfile(Context context, long id, int active,
+			long seconds) {
+		Intent intent = new Intent(ProfileSwitcherConstants.ACTION_SET_PROFILE);
+		intent.putExtra(Profile.COLUMN_ID, id);
+		intent.putExtra(Profile.COLUMN_ACTIVE, active);
+		intent.putExtra(Profile.COLUMN_ACTIVATE_TIME,
+				System.currentTimeMillis());
+		intent.putExtra(Profile.COLUMN_EXPIRE_TIME, seconds);
+
+		context.startService(intent);
+	}
+
+	public static float getLocationDistancePreference() {
+		// TODO Load location distance from preference
+		return 50;
+	}
+
+	public static int getLocationUpdateTimePreference() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+	public static float getLocationUpdateDistancePreference() {
+		// TODO Auto-generated method stub
+		return 500;
+	}
+
+}
