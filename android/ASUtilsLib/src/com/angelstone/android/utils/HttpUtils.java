8620b47281916489207607d33c75ded7a911fbdc
Jingnan Si (jingnan si <Jingnan Si (jingnan.si@gmail.com)>
2011-04-12 17:36:59 +0800

    add socks proxy support

--- android/ASUtilsLib/src/com/angelstone/android/utils/HttpUtils.java
+++ android/ASUtilsLib/src/com/angelstone/android/utils/HttpUtils.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
@@ -14,25 +18,30 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
 import android.content.Context;
 
+import com.angelstone.android.net.ProxySocketFactory;
+
 public class HttpUtils {
 	public static final int REGISTRATION_TIMEOUT = 60 * 1000; // ms
 
-	public static String postData(Context context, String url, String paramName,
-			String uploadData) throws IOException {
+	public static String postData(Context context, String url,
+			String paramName, String uploadData) throws IOException {
 		HttpResponse resp;
 
 		final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair(paramName, uploadData));
 		HttpEntity entity = null;
-		entity = new UrlEncodedFormEntity(params,"UTF-8");
+		entity = new UrlEncodedFormEntity(params, "UTF-8");
 
 		HttpPost post = new HttpPost(url);
 		post.addHeader(entity.getContentType());
@@ -49,17 +58,13 @@ public class HttpUtils {
 		}
 	}
 
-	public static String postData(Context context, String url, String paramName,
-			byte[] uploadData) throws IOException {
+	public static String postData(Context context, String postUrl,
+			String paramName, byte[] uploadData) throws IOException {
 		HttpResponse resp;
 
-		//final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-		//params.add(new BasicNameValuePair(paramName, HtmuploadData));
-		HttpEntity entity = null;
-		//entity = new UrlEncodedFormEntity(params,"UTF-8");
-		entity = new ByteArrayEntity(uploadData);
+		HttpEntity entity = new ByteArrayEntity(uploadData);
 
-		HttpPost post = new HttpPost(url);
+		HttpPost post = new HttpPost(postUrl);
 		post.addHeader(entity.getContentType());
 		post.setEntity(entity);
 		HttpClient mHttpClient = null;
@@ -75,8 +80,12 @@ public class HttpUtils {
 	}
 
 	private static String getResponse(HttpResponse response) throws IOException {
-		String result = "";
 		InputStream in = response.getEntity().getContent();
+		return readResponseString(in);
+	}
+
+	private static String readResponseString(InputStream in) throws IOException {
+		String result = "";
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		StringBuilder str = new StringBuilder();
 		String line = null;
@@ -92,14 +101,31 @@ public class HttpUtils {
 
 	/**
 	 * Configures the httpClient to connect to the URL provided.
+	 * @throws UnknownHostException 
 	 */
-	private static HttpClient createHttpClient() {
+	private static HttpClient createHttpClient() throws UnknownHostException {
+		SchemeRegistry registry = registerCustomizedSocketFactory();
+
 		HttpClient mHttpClient = new DefaultHttpClient();
 		final HttpParams params = mHttpClient.getParams();
 		HttpConnectionParams.setConnectionTimeout(params, REGISTRATION_TIMEOUT);
 		HttpConnectionParams.setSoTimeout(params, REGISTRATION_TIMEOUT);
 		ConnManagerParams.setTimeout(params, REGISTRATION_TIMEOUT);
 
+		mHttpClient = new DefaultHttpClient(new ThreadSafeClientConnManager(
+				params, registry), params);
+
 		return mHttpClient;
+	}
+
+	private static SchemeRegistry registerCustomizedSocketFactory() throws UnknownHostException {
+		Proxy proxy = new Proxy(Proxy.Type.SOCKS, 
+				new InetSocketAddress(InetAddress.getLocalHost(), 18080));
+		
+		Scheme http = new Scheme("http", new ProxySocketFactory(proxy), 80);
+
+		SchemeRegistry sr = new SchemeRegistry();
+		sr.register(http);
+		return sr;
 	}
 }
